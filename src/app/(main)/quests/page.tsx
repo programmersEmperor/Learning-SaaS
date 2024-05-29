f006db52977c0998199a5c599bc6caec4e3c7cdb
programmersEmperor <mutasim.business2020@gmail.com>
2024-05-29 23:04:08 +0300

    create the quests page

--- src/app/(main)/quests/page.tsx
+++ src/app/(main)/quests/page.tsx
@@ -1,12 +1,33 @@
 import FeedWrapper from "@/components/feedWrapper";
 import StickyWrapper from "@/components/stickyWrapper";
-import { AvatarImage } from "@/components/ui/avatar";
+import { Progress } from "@/components/ui/progress";
 import UserProgress from "@/components/userProgress";
 import { auth } from "@clerk/nextjs/server";
-import { Avatar } from "@radix-ui/react-avatar";
-import { Separator } from "@radix-ui/react-separator";
 import Image from "next/image";
 import { redirect } from "next/navigation";
+
+const quests = [
+    {
+        title: 'Earn 20 XP',
+        value: 20
+    },
+    {
+        title: 'Earn 50 XP',
+        value: 50
+    },
+    {
+        title: 'Earn 100 XP',
+        value: 100
+    },
+    {
+        title: 'Earn 500 XP',
+        value: 500
+    },
+    {
+        title: 'Earn 1000 XP',
+        value: 1000
+    },
+]
 
 export default async function Quests(){
     const {getToken} = await auth()
@@ -22,37 +43,33 @@ export default async function Quests(){
     const userSubscriptionData = userSubscriptionJson.result
     const isPro = !!userSubscriptionData?.isActive;    
 
-    // const responseTopLearners = await fetch("http://localhost:3000/api/topLearners", {headers: { Authorization: `Bearer ${await getToken()}`}})
-    // const topLearnersJson = await responseTopLearners.json()
-    // const topLearnersData = topLearnersJson.result
-    // if(topLearnersData){
-    //     redirect('/courses')
-    // }
-
     return <div className="flex flex-row-reverse gaq-[48px] px-6">
         <StickyWrapper>
             <UserProgress activeCourse={userProgress.activeCourse}  hearts={userProgress.hearts} points={userProgress.points} hasActiveSubscription={isPro} />
         </StickyWrapper>
         <FeedWrapper>
             <div className="w-full flex flex-col items-center">
-                <Image src={'/assets/leaderboard.svg'}  alt="Leaderboard" width={90} height={90}/>
+                <Image src={'/assets/quests.svg'}  alt="Quests" width={90} height={90}/>
             </div>
             <h1 className="text-center font-bold text-neutral-800 text-2xl my-6">
-                Leaderboard
+                Quests
             </h1>
             <p className="text-muted-foreground text-center text-lg mb-6">
-                See where you stand among other learners in the community.
+                Complete quests by earning points
             </p>
-            <Separator className="mb-4 h-0.5 rounded-full" />
-            {topLearnersData.map((userProgress: any, index: number)=> 
-            <div key={userProgress.userId} className="flex items-center w-full p-2  px-4 rounded-xl hover:bg-gray-200/50">
-                <p className="font-bold text-lime-700 mr-4">{index + 1}</p>
-                <Avatar className="border bg-green-500 h-12 w-12 ml-3 mr-6">
-                    <AvatarImage className="object-cover" src={userProgress.userImageSrc}/>
-                </Avatar>
-                <p className="font-bold text-neutral-800 flex-1">{userProgress.userName}</p>
-                <p className="text-muted-foreground">{userProgress.points} XP</p>
-            </div>)}
+            <ul className="w-full">
+                {quests.map((quest)=>{
+                    const progress = (userProgress.points / quest.value) * 100
+
+                    return <div key={quest.title} className="flex items-center w-full p-4 gap-x-4 border-t-2">
+                        <Image src={'/assets/points.svg'} alt={'Points'} width={60} height={60} />
+                        <div className="flex flex-col gap-y-2 w-full">
+                            <p className="text-neutral-700 text-xl font-bold">{quest.title}</p>
+                            <Progress className={'h-3'} value={progress} />
+                        </div>
+                    </div>
+                })}
+            </ul>
         </FeedWrapper>
     </div>
 }
