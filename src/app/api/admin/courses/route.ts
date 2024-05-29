93a1cba59c4f6e0ffef2214ceabed6185d57fc45
programmersEmperor <mutasim.business2020@gmail.com>
2024-05-30 02:37:25 +0300

    finish the project

--- src/app/api/admin/courses/route.ts
+++ src/app/api/admin/courses/route.ts
@@ -9,3 +9,12 @@ export async function GET(req: NextRequest){
     const courses = await db.query.courses.findMany();
     return new NextResponse(JSON.stringify(courses))
 }
+
+export async function POST(req: NextRequest){
+    const body = await req.json();
+    const data = await db.insert(courses).values({
+        ...body,
+    }).returning();
+
+    return NextResponse.json(data[0])
+}
