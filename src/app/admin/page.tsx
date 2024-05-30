b54faf3f26dc83e5925938de22341e68c7af1e8f
programmersEmperor <mutasim.business2020@gmail.com>
2024-05-31 02:50:44 +0300

    add a comment

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 const App = dynamic(()=> import('./app'), {ssr: false}) 
-// just some random comment
+// just some random comment sd
 export default function Admin(){
     return <App/>
 }
